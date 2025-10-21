d22d75851f04e69cbcdd883f6009717831693977
ohchanwpc <ohchanwpc@gmail.com>
2025-10-22 05:13:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ st.markdown(
         ステップ4: キーワード（例：川、家族、影）を入力してください
         </p>
     </div>
-    <div style="margin-top: 1.2em; color: #666; font-size: 13px; line-height: 1.5;">
+    <div style="margin-top: 0em; color: #666; font-size: 13px; line-height: 1.5;">
         ※ 一茶の約2.2万句から検索するため、<b>「単語」入力のほうが多くの俳句がヒットします。</b><br>
         （文章を入れると条件に合う句が少なくなる場合があります）
     </div>
